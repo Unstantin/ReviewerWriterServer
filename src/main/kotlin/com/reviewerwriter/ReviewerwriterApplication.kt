aebcc26695aae67f907283dc2fbf0671fa210439
Unstantin <samoylovich2005@gmail.com>
2024-02-18 14:42:41 +0300

    Initial commit

---
+++ src/main/kotlin/com/reviewerwriter/ReviewerwriterApplication.kt
@@ -0,0 +1,11 @@
+package com.reviewerwriter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReviewerwriterApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReviewerwriterApplication>(*args)
+}
---
+++ src/test/kotlin/com/reviewerwriter/ReviewerwriterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.reviewerwriter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReviewerwriterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
